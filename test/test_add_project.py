067ab07b9b6b2fcc096f88de31bf8d6a04619d7d
Dmitry Efremov <kykareky05@mail.ru>
2018-11-19 01:10:31 +0300

    не выходит добавление проекта через soap, удаление проходит

--- test/test_add_project.py
+++ test/test_add_project.py
@@ -3,10 +3,10 @@ from model.project import Project
 
 
 def test_add_project(app, db):
-    # username = app.config["webadmin"]["username"]
-    # password = app.config["webadmin"]["password"]
+    username = app.config["webadmin"]["username"]
+    password = app.config["webadmin"]["password"]
     app.session.login("administrator", "root")
-    # old_list = app.soap.get_list_project(username, password)
+    old_list = app.soap.get_list_project(username, password)
     # data = Project(name="test")
     app.project.open_create_project_page()
 
@@ -14,7 +14,8 @@ def test_add_project(app, db):
 
     app.project.add_new_project()
 
-    # old_list.append(Project(name="test"))
-    # new_list = app.soap.get_list_project(username, password)
-    # print(new_list)
+    # old_list.append(project)
+    new_list = app.soap.get_list_project(username, password)
+    print(new_list)
+    assert len(old_list) + 1 == len(new_list)
     # assert sorted(old_list, key=Project.id_or_max) == sorted(new_list, key=Project.id_or_max)
