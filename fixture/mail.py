578763be378b4b56503926dc11cf150ecb24d0c6
Dmitry Efremov <kykareky05@mail.ru>
2018-11-18 17:32:33 +0300

    Simple Object Access Protocol (SOAP)

--- fixture/mail.py
+++ fixture/mail.py
@@ -22,6 +22,6 @@ class MailHelper:
                         pop.dele(n+1)
                         pop.quit()
                         return msg.get_payload()
-            pop.quit() # или close?
+            pop.quit()
             time.sleep(7)
         return None
